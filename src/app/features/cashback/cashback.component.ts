f93f444738c624858a252867a896c3a8f404cf7c
phyolwinlwinlatt <phyolwinlwinlatt@gmail.com>
2024-10-13 21:50:40 +0700

    feat: cashback module

---
+++ src/app/features/cashback/cashback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cashback',
+  standalone: true,
+  imports: [],
+  templateUrl: './cashback.component.html',
+  styleUrl: './cashback.component.css'
+})
+export class CashbackComponent {
+
+}
